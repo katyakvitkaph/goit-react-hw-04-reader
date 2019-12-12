774de72a0af137980980404d59f8a17f301a5e6b
katyakvitkaph <kvitka.katja@gmail.com>
2019-12-12 17:50:00 +0200

    bld

---
+++ build/precache-manifest.4b853a2ebfc8ad86e38dfe9951c1860b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4fcca08a21c78178b97db8936a9f8e1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d0182f24f0f137d5ed22",
+    "url": "./static/css/main.f5321b82.chunk.css"
+  },
+  {
+    "revision": "3c9e6143c8b8e9665656",
+    "url": "./static/js/2.8dc5210d.chunk.js"
+  },
+  {
+    "revision": "d0182f24f0f137d5ed22",
+    "url": "./static/js/main.e8a18340.chunk.js"
+  },
+  {
+    "revision": "155fbd6f8f8a14aef1cc",
+    "url": "./static/js/runtime-main.8958cbb6.js"
+  }
+]);
